0f6dc8180938a9f457769e5d3c8f881b78583cdb
SamadheeSadeesha <ssadeesha11@gmail.com>
2025-10-06 14:47:15 +0530

    Fix svg abd dependency mismatch issues

--- app/login.tsx
+++ app/login.tsx
@@ -33,7 +33,7 @@ export default function Login() {
     <SafeAreaView className="flex-1 bg-white">
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <Image
-          source={require("../assets/images/logo.svg")}
+          source={require("../assets/images/logo.png")}
           className="w-[200px] h-[200px]"
           resizeMode="contain"
         />
